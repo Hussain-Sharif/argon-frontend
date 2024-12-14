640a32437ac160cad5f8317bc46c7eb31df7f2bc
Hussain-Sharif <99sharif00@gmail.com>
2024-12-14 14:36:23 +0530

    Setup for the Porject with Shadcn and Vite-JS

---
+++ src/components/Login/index.jsx
@@ -0,0 +1,17 @@
+
+import { Button } from "@/components/ui/button"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
+
+export const Login=()=>(
+        <div>
+            <Avatar>
+  <AvatarImage src="https://github.com/shadcn.png" />
+  <AvatarFallback>CN</AvatarFallback>
+</Avatar>
+
+            hi Sharif<br/>
+            <Button variant="outline">Button</Button>
+
+        </div>
+    )
