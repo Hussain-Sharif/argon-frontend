930aa12276148ada87f80fd02c4fb55f70219c73
Hussain_Sharif <99sharif00@gmail.com>
2024-12-22 15:47:57 +0530

    fixed lazy loading issue and added New image

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,56 @@
-import { Suspense,lazy } from 'react'
 import 'animate.css';
+import { Suspense,lazy } from 'react'
 import { Route, BrowserRouter, Routes, } from 'react-router-dom'
+import { LoadingAnime } from './app/Loader/loadingAnime';
 import './App.css'
-import { Login } from './components/Login'
+// import { Login } from './components/Login'
 import { ReUseDiv } from './components/ReusableStyledComponents'
-import { Home } from './components/Home'
-import { Register } from './components/Register'
+import { Button } from './components/ui/button';
+// import { Home } from './components/Home'
+// import { Register } from './components/Register'
 
+const Login = lazy(() => import('./components/Login').then(module => ({ default: module.Login })))
+const Register = lazy(() => import('./components/Register').then(module => ({ default: module.Register })))
+const Home = lazy(() => import('./components/Home').then(module => ({ default: module.Home })))
 
+// function App() {
+//   return (
+//       <BrowserRouter >
+        
+//           <Routes>
+          
+//             <Route  path="/" element={<Suspense fallback={<LoadingAnime/>}><Home/></Suspense>}/>
+//             <Route  path='/login' element={<Suspense fallback={<LoadingAnime/>}><Login/></Suspense>}/>
+//             <Route  path="/register" element={<Suspense fallback={<LoadingAnime/>}><Register/></Suspense>}/>        
+//             <Route path='*' element={<NotFound/>}/>
+//           </Routes>
+        
+//       </BrowserRouter>
+//   )
+// }
 
 function App() {
   return (
-    <ReUseDiv w="100%" h="100%">
-      {/* <Suspense fallback={<LoadingAnime></LoadingAnime>}> */}
-      <BrowserRouter>
-        
-          <Routes>
-          
-            <Route exact path="/" element={<Home/>}/>
-            <Route exact path='/login' element={<Login/>}/>
-            <Route exact path="/register" element={<Register/>}/>        
-          
-          </Routes>
-        
-      </BrowserRouter>
-      {/* </Suspense> */}
-    </ReUseDiv>
+    <BrowserRouter>
+      <Suspense fallback={<LoadingAnime />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  )
+}
+
+const NotFound=()=>{
+  return(
+    <div className='h-screen w-full flex flex-col justify-center items-center'>
+      <img className='max-w-72' alt="argon_logo" src="https://s3-alpha-sig.figma.com/img/a7c2/c95a/e57df5f03ddceb5a4011eb1efd953170?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=i7lvM1s0i74MfggIel4C7VrexlGfffkxShALGiS99Txtnir6kf9cE0wDtpfwUiHXVw0BRZQHQIfzxZw1EQ0H1ClfCtFjay3rQCAIS~nSYjLOjg2TBlpcxwCRke5MQCppl-mEDFaqK3aVya7rCuiCkN2hntF9bpWkY8IHJUAJHnAT3XawIK1KXfQdPSW8NrdCJX0OPs0WWHm1JoUY~g374aVLigYQeWKFPHWCLbp7LdkPNRoL~08fbV~qj1viBUfox9ktE9IfCzq6zwxjitIwcAaYxJhH7o77FBChfuYVChd6aYDxaStNv9~tWtzOPwKUir3zVend3~SPe9Jc-FP5VA__"/>
+      <h1 className='text-4xl mb-2 mt-2 font-bold text-center'>404 Page Not Found</h1>
+      <Button type="button" onClick={()=>{window.location.href="/"}}>Go to Home Page</Button>
+    </div>
   )
 }
 
--- src/components/AllAppliance/index.jsx
+++ src/components/AllAppliance/index.jsx
@@ -5,38 +5,42 @@ export const AllAppliance=(props)=>{
     const {allAppliancesList}=props
 
     return(
+        <>
         <div className="md:p-10 md:pb-[0px] p-5 pb-0 md:max-h-[90vh] bg-[#F5FEFF]">
             <div className="md:top-[-250px] top-[-170px] z-40 relative w-[100%] bg-white p-10 rounded-lg border shadow-xl
             animate__animated animate__fadeInUp">
                 <h1 className="text-4xl mb-2 font-bold">All Appliances</h1>
                 <p className="text-base mb-7 text-[#525F7F]">The time is now for it to be okay to be great. For being a bright color. For standing out.</p>
-            <div className="justify-center items-start" style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
-                {allAppliancesList.map((appliance) => (
-                    <div
-                    key={appliance.id}
-                    className="min-w-72 max-w-80 flex flex-col justify-start items-start"
-                    style={{
-                        
-                        borderRadius: "8px",
-                        padding: "10px",
-                        textAlign: "center",
-                        width: "200px",
-                    }}
-                    >
-                    {/* Render the Lucide Icon */}
-                    <div className="mb-4 bg-[#bde4f4bf] rounded-full p-4">{appliance.image()}</div>
+                <div className="justify-center items-start" style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
+                    {allAppliancesList.map((appliance) => (
+                        <div
+                        key={appliance.id}
+                        className="min-w-72 max-w-80 flex flex-col justify-start items-start"
+                        style={{
+                            
+                            borderRadius: "8px",
+                            padding: "10px",
+                            textAlign: "center",
+                            width: "200px",
+                        }}
+                        >
+                        {/* Render the Lucide Icon */}
+                        <div className="mb-4 bg-[#bde4f4bf] rounded-full p-4">{appliance.image()}</div>
 
-                    {/* Appliance Name */}
-                    <h3 className="text-xl mb-2">{appliance.name}</h3>
+                        {/* Appliance Name */}
+                        <h3 className="text-xl mb-2">{appliance.name}</h3>
 
-                    {/* Appliance Description */}
-                    <p className="text-[#525F7F] text-sm text-left">
-                        {appliance.description}
-                    </p>
-                    </div>
-                ))}
+                        {/* Appliance Description */}
+                        <p className="text-[#525F7F] text-sm text-left">
+                            {appliance.description}
+                        </p>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
-    )
+        {/* <div className="w-full mb-0 relative border-0 outline-none bg-[#f5feff]">
+                <img className="w-full h-[220px] border-0 " alt="up-curve" src="assests/upper-curve.png"/>
+        </div> */}
+   </> )
 }
